51cdfc1dbcd85eea21c12107750fae71da03f6d0
Pavel Kalugin <pkfrance@yahoo.com>
2026-07-13 18:38:50 +0200

    Avoiding potential bug in case of type promotion

--- src/algorithms/utils.jl
+++ src/algorithms/utils.jl
@@ -6,6 +6,6 @@
 Return the unit-modulus complex number with the same phase as `z` (`z / abs(z)`),
 or `one(z)` when `z` is zero, where the phase is undefined.
 """
-@inline function safe_phase(z::Complex{T})::Complex{T} where {T}
+@inline function safe_phase(z::Complex{T})::Complex{T} where {T<:AbstractFloat}
     return iszero(z) ? one(z) : z / abs(z)
 end
